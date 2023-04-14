e7f5edd00ff0bb227c9d017e20d525ac44e976ac
MilenaTrajanoska <68651081+MilenaTrajanoska@users.noreply.github.com>
2023-04-14 18:30:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,22 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
+    """
+        Function which returns a simple dictionary
+
+        Returns:
+            dict
+    """
     return {"Hello": "World"}
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int, query: str = None):
+    """
+        Function which returns a dictionary with the passed arguments
+        Params:
+            item_id: int - id of the item
+            query: str - query string
+        Returns:
+            dict
+    """
+    return {"item_id": item_id, "query": query}
