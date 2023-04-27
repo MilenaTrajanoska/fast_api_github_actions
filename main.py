2ff4a98b49ebfede729d9260fd244c5a648bc77a
trajanoska28 <trajanoska28@gmail.com>
2023-04-27 16:53:16 +0200

    Updated main

--- main.py
+++ main.py
@@ -5,10 +5,21 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
+    """
+        Function which returns a simple dictionary
+
+        Returns:
+            dict
+    """
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, query: str = None):
+    """
+        Function which returns a simple dictionary
 
+        Returns:
+            dict
+    """
     return {"item_id": item_id, "query": query}
